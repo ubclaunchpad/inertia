c883ab82093f30e8f04b775928698d4c903c05a0
chadlagore <chad.d.lagore@gmail.com>
2017-12-09 18:42:24 -0800

    POC

---
+++ poc/wrapper/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+var pemFileLoc = "/Users/chadlagore/.ssh/id_inertia"
+
+func main() {
+	host := os.Args[1]
+	remoteCmd := os.Args[2]
+
+	result, err := RunSSHCommand(host, remoteCmd)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(result)
+}
+
+// RunSSHCommand runs a command remotely.
+func RunSSHCommand(host, remoteCmd string) (*bytes.Buffer, error) {
+	cmd := exec.Command(
+		"ssh", "-i", pemFileLoc,
+		"-t", host, remoteCmd,
+	)
+
+	// Capture result.
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	err := cmd.Run()
+
+	return &out, err
+}
