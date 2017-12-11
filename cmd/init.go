4eb3444b95aab0c1c13df375ff46c7eb5f301e36
Chad Lagore <chad.d.lagore@gmail.com>
2017-12-10 19:52:31 -0800

    Away we gooooo....

---
+++ cmd/init.go
@@ -0,0 +1,49 @@
+// Copyright © 2017 NAME HERE team@ubclaunchpad.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// initCmd represents the init command
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Bootstraps a server for continuous deployment",
+	Long: `Bootstraps a server for continuous deployment by installing the
+Inertia daemon and other dependencies. You will need SSH access to
+the server to initialize.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("init called")
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(initCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	initCmd.Flags().StringP("host", "H", "", "IP address of the host VPS")
+	initCmd.Flags().StringP("user", "u", "root", "User for SSH access")
+	initCmd.Flags().StringP("identity", "i", "$HOME/.ssh/id_rsa", "PEM file location")
+}
