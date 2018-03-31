ae9f003847e94f2b77e6e483443e6d918912e006
Robert Lin <robertlin1@gmail.com>
2018-03-31 12:15:28 -0700

    Report correct secret in remote add, remove redundant var

--- common/request.go
+++ common/request.go
@@ -1,9 +1,6 @@
 package common
 
 const (
-	// DefaultSecret used for some verification
-	DefaultSecret = "inertia"
-
 	// DaemonOkResp is the OK response upon successfully reaching daemon
 	DaemonOkResp = "I'm a little Webhook, short and stout!"
 
