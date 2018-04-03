fb19d1daf26a81d2d2cd97a94d488d2ffa3d617d
John Lee <jlee14c@hotmail.com>
2018-04-03 11:56:22 -0700

    tabs to 2 spaces

--- daemon/web/components/App.js
+++ daemon/web/components/App.js
@@ -4,76 +4,76 @@ import PropTypes from 'prop-types';
 import InertiaClient from '../client';
 
 export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: "",
-            password: "",
-            loginAlert: ""
-        };
-        this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
-        this.handleUsernameBlur = this.handleUsernameBlur.bind(this);
-        this.handlePasswordBlur = this.handlePasswordBlur.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      loginAlert: ""
+    };
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+    this.handleUsernameBlur = this.handleUsernameBlur.bind(this);
+    this.handlePasswordBlur = this.handlePasswordBlur.bind(this);
+  }
 
-    async handleLoginSubmit() {
-        const endpoint = 'https://localhost:8081/web/login';
-        const params = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                Username: this.state.username,
-                Password: this.state.password
-            })
-        };
+  async handleLoginSubmit() {
+    const endpoint = 'https://localhost:8081/web/login';
+    const params = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        Username: this.state.username,
+        Password: this.state.password
+      })
+    };
 
-        const response = await this.props.client._post(endpoint, params).json();
-        this.setState({ loginAlert: response.body });
-    }
+    const response = await this.props.client._post(endpoint, params).json();
+    this.setState({loginAlert: response.body});
+  }
 
-    handleUsernameBlur(e) {
-        this.setState({ username: e.target.value });
-    }
+  handleUsernameBlur(e) {
+    this.setState({username: e.target.value});
+  }
 
-    handlePasswordBlur(e) {
-        this.setState({ password: e.target.value });
-    }
+  handlePasswordBlur(e) {
+    this.setState({password: e.target.value});
+  }
 
-    render() {
-        return (
-            <div>
-                <p align="center">
-                    <img
-                        src="https://github.com/ubclaunchpad/inertia/blob/master/.static/inertia-with-name.png?raw=true"
-                        width="10%"/>
-                </p>
-                <p align="center">
-                    This is the Inertia web client!
-                </p>
-                <div style={styles.login}>
-                    <input onBlur={this.handleUsernameBlur} placeholder="Username" />
-                    <input onBlur={this.handlePasswordBlur} placeholder="Password" />
-                    <button onClick={this.handleLoginSubmit}>Login</button>
-                    <p>{this.state.loginAlert}</p>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <p align="center">
+          <img
+            src="https://github.com/ubclaunchpad/inertia/blob/master/.static/inertia-with-name.png?raw=true"
+            width="10%"/>
+        </p>
+        <p align="center">
+          This is the Inertia web client!
+        </p>
+        <div style={styles.login}>
+          <input onBlur={this.handleUsernameBlur} placeholder="Username"/>
+          <input onBlur={this.handlePasswordBlur} placeholder="Password"/>
+          <button onClick={this.handleLoginSubmit}>Login</button>
+          <p>{this.state.loginAlert}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 App.propTypes = {
-    client: PropTypes.instanceOf(InertiaClient)
+  client: PropTypes.instanceOf(InertiaClient)
 }
 
 const styles = {
-    container: {
-        display: 'flex',
-    },
-    login: {
-        display: 'flex',
-        flexFlow: 'column',
-        alignItems: 'center'
-    }
+  container: {
+    display: 'flex',
+  },
+  login: {
+    display: 'flex',
+    flexFlow: 'column',
+    alignItems: 'center'
+  }
 };
--- daemon/web/webpack.config.js
+++ daemon/web/webpack.config.js
@@ -3,48 +3,48 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
-    mode: 'development',
-    entry: './index.js',
-    output: {
-        path: `${__dirname}/public/`,
-        filename: 'bundle.js',
-    },
-    devServer: {
-        port: 7900,
-        inline: true,
-        contentBase: './public',
-        publicPath: '/',
-    },
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: '/node_modules/',
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['es2015', 'react', 'stage-3'],
-                        },
-                    }
-                ],
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    path: `${__dirname}/public/`,
+    filename: 'bundle.js',
+  },
+  devServer: {
+    port: 7900,
+    inline: true,
+    contentBase: './public',
+    publicPath: '/',
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: '/node_modules/',
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015', 'react', 'stage-3'],
             },
+          }
         ],
-    },
-    plugins: [
-        new webpack.EnvironmentPlugin(['NODE_ENV']),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.DefinePlugin({
-            // suppress react devtools console warning
-            '__REACT_DEVTOOLS_GLOBAL_HOOK__': '({ isDisabled: true })'
-        }),
-        new HtmlWebpackPlugin({
-            template: './index.html',
-            filename: 'index.html',
-            inject: 'body',
-        })
-    ]
+      },
+    ],
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin(['NODE_ENV']),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      // suppress react devtools console warning
+      '__REACT_DEVTOOLS_GLOBAL_HOOK__': '({ isDisabled: true })'
+    }),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+      inject: 'body',
+    })
+  ]
 };
 
 module.exports = config;
