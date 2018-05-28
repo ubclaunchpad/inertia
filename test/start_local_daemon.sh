8be24f7d804aea325cebf5cf8b3e2e558002573a
Robert Lin <robertlin1@gmail.com>
2018-05-27 17:16:06 -0700

    Remove redundant mkdir command

--- test/start_local_daemon.sh
+++ test/start_local_daemon.sh
@@ -5,8 +5,6 @@ go build ./daemon/inertiad
 
 echo "Daemon Token:" $(./inertiad token $(pwd)/test/keys/id_rsa)
 
-mkdir inertia_local
-
 ./inertiad run 127.0.0.1 \
     $(pwd)/test/keys/id_rsa \
     $(pwd)/test/certs/ \
