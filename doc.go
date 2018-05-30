3420b55cd35121470b0c63214d9dc5009d8332b9
Robert Lin <robertlin1@gmail.com>
2018-05-29 22:52:20 -0700

    Add inertia command godoc

---
+++ doc.go
@@ -0,0 +1,58 @@
+/*
+
+Inertia is the command line interface that helps you set up your remote for
+continuous deployment and allows you to manage your deployment through
+configuration options and various commands. It can be installed in several ways:
+
+	# Mac users
+	brew install ubclaunchpad/inertia
+
+	# Windows users
+	scoop bucket add ubclaunchpad https://github.com/ubclaunchpad/scoop-bucket
+	scoop install inertia
+
+Users of other platforms can install the Inertia CLI from the Releases page,
+found here: https://github.com/ubclaunchpad/inertia/releases/latest
+
+To help with usage, most relevant documentation can be seen by using the --help
+flag on any command:
+
+	inertia --help
+	inertia init --help
+	inertia [REMOTE] up --help
+
+Documentation can also be triggered by simply entering a command without the
+prerequisite arguments or additional commands:
+
+	inertia remote        # documentation about remote configuration commands
+
+Inertia has two "core" sets of commands - one that primarily handles local
+configuration, and one that allows you to control your remote VPS instances and
+their associated deployments.
+
+For local configuration, most commands will build off of the root "inertia ..."
+command. For example, a typical set of commands to set up a project might look
+like:
+
+	inertia init                 # initiates Inertia configuration
+	inertia remote add my_cloud  # adds configuration for a remote VPS instance
+
+The other set of commands are based on a remote VPS configuration, and the
+available commands can be seen by running:
+
+	inertia [REMOTE] --help
+
+In the previous example, the next steps to set up a deployment might be:
+
+	inertia my_cloud init        # bootstraps remote and installs Inertia daemon
+	inertia my_cloud up          # deploys your project
+
+Some of these commands offer a --stream flag that allows you to view realtime
+log feedback from the daemon.
+
+More documentation on Inertia, how it works, and how to use it can be found
+in the project repository: https://github.com/ubclaunchpad/inertia/tree/master
+
+*/
+
+package main
