3563d4b86c95a60df3c9f652b693383fe2ec4361
Robert Lin <robertlin1@gmail.com>
2018-06-06 20:44:48 -0700

    User readonly op to list users

--- daemon/inertiad/auth/users.go
+++ daemon/inertiad/auth/users.go
@@ -101,7 +101,7 @@ func (m *userManager) RemoveUser(username string) error {
 // UserList returns a list of all registered users
 func (m *userManager) UserList() []string {
 	userList := make([]string, 0)
-	m.db.Update(func(tx *bolt.Tx) error {
+	m.db.View(func(tx *bolt.Tx) error {
 		users := tx.Bucket(m.usersBucket)
 		return users.ForEach(func(username, v []byte) error {
 			userList = append(userList, string(username))
