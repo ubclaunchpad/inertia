e78926f4141baf87bff2381a0249e04f1e1f47f5
Robert Lin <robertlin1@gmail.com>
2018-06-20 19:24:34 -0700

    Remove go-git dependencies in CLI
    Most Inertia users will have git installed - no need to package a complete git implementation alongside the CLI. This change replaces previous go-git functions with shell commands.

---
+++ local/git.go
@@ -0,0 +1,48 @@
+package local
+
+import (
+	"errors"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+// GetRepoRemote reads a remote URL
+func GetRepoRemote(remote string) (string, error) {
+	arg := "remote." + remote + ".url"
+	out, err := exec.Command("git", "config", "--get", arg).Output()
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(out)), nil
+}
+
+// GetRepoCurrentBranch returns the current repository branch
+func GetRepoCurrentBranch() (string, error) {
+	out, err := exec.Command("git", "symbolic-ref", "--short", "HEAD").Output()
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(out)), nil
+}
+
+// checkForGit returns an error if we're not in a git repository.
+func checkForGit(cwd string) error {
+	// Quick failure if no .git folder.
+	gitFolder := filepath.Join(cwd, ".git")
+	if _, err := os.Stat(gitFolder); os.IsNotExist(err) {
+		return errors.New("this does not appear to be a git repository")
+	}
+
+	// Also fail if no origin detected
+	url, err := GetRepoRemote("origin")
+	if err != nil {
+		return err
+	}
+	if url == "" {
+		return errors.New("no remote origin set")
+	}
+
+	return nil
+}
