5e9a1d265e71962625da111278d824a8ca64b49a
Robert Lin <robertlin1@gmail.com>
2018-06-23 21:38:18 -0700

    Allow variable config path via --config flag (#275)
    * Allow variable config path via --config flag

--- local/storage_test.go
+++ local/storage_test.go
@@ -9,25 +9,25 @@ import (
 )
 
 func TestInitializeInertiaProjetFail(t *testing.T) {
-	err := InitializeInertiaProject("", "", "")
+	err := InitializeInertiaProject("inertia.toml", "", "", "")
 	assert.NotNil(t, err)
 }
 
 func TestGetConfigFail(t *testing.T) {
-	_, _, err := GetProjectConfigFromDisk()
+	_, _, err := GetProjectConfigFromDisk("inertia.toml")
 	assert.NotNil(t, err)
 }
 
 func TestConfigCreateAndWriteAndRead(t *testing.T) {
-	err := createConfigFile("test", "dockerfile", "")
+	err := createConfigFile("inertia.toml", "test", "dockerfile", "")
 	assert.Nil(t, err)
 
 	// Already exists
-	err = createConfigFile("test", "dockerfile", "")
+	err = createConfigFile("inertia.toml", "test", "dockerfile", "")
 	assert.NotNil(t, err)
 
 	// Get config and add remotes
-	config, configPath, err := GetProjectConfigFromDisk()
+	config, configPath, err := GetProjectConfigFromDisk("inertia.toml")
 	assert.Nil(t, err)
 	config.AddRemote(&cfg.RemoteVPS{
 		Name:    "test",
@@ -55,17 +55,17 @@ func TestConfigCreateAndWriteAndRead(t *testing.T) {
 	assert.Nil(t, err)
 
 	// Test config read
-	readConfig, _, err := GetProjectConfigFromDisk()
+	readConfig, _, err := GetProjectConfigFromDisk("inertia.toml")
 	assert.Nil(t, err)
 	assert.Equal(t, config.Remotes[0], readConfig.Remotes[0])
 	assert.Equal(t, config.Remotes[1], readConfig.Remotes[1])
 
 	// Test client read
-	client, err := GetClient("test2")
+	client, err := GetClient("test2", "inertia.toml")
 	assert.Nil(t, err)
 	assert.Equal(t, "test2", client.Name)
 	assert.Equal(t, "12343:80801", client.GetIPAndPort())
-	_, err = GetClient("asdf")
+	_, err = GetClient("asdf", "inertia.toml")
 	assert.NotNil(t, err)
 
 	// Test config remove
