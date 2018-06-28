921ff6f1feb9b4db594b9234133836e6f79452f2
Robert Lin <robertlin1@gmail.com>
2018-06-27 19:08:51 -0700

    Remove nonfunctional test

--- provision/ec2_test.go
+++ provision/ec2_test.go
@@ -15,12 +15,3 @@ func TestNewEC2ProvisionerFromEnv(t *testing.T) {
 	_, err := NewEC2Provisioner("id", "key")
 	assert.NotNil(t, err)
 }
-
-func TestListImageOptionsNoAuth(t *testing.T) {
-	prov, err := NewEC2Provisioner("id", "key")
-	assert.NotNil(t, prov.client.Config.Credentials)
-
-	_, err = prov.ListImageOptions("us-east-1")
-	assert.NotNil(t, err)
-	assert.Contains(t, err.Error(), "AuthFailure")
-}
