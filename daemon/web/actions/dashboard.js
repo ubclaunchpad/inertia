2af1f0feda6996ec603e40849349ebad575e3d71
asitu <annasitu1@gmail.com>
2018-06-28 20:14:51 -0700

    Fixed lint issues

--- daemon/web/actions/dashboard.js
+++ daemon/web/actions/dashboard.js
@@ -5,7 +5,6 @@ import {
 
 export const testAction = payload => (dispatch) => {
   // remove later
-  console.log('dashboard action fired!');
   dispatch({
     type: TEST_DASHBOARD_ACTION,
     payload,
--- daemon/web/components/IconHeader/IconHeader.js
+++ daemon/web/components/IconHeader/IconHeader.js
@@ -3,21 +3,22 @@ import PropTypes from 'prop-types';
 import './index.sass';
 
 const icons = {
-    dashboard: <i className="fas fa-th-large"></i>,
-    containers: <i className="fas fa-database fa-lg"></i>,
-    team: <i className="fas fa-users"></i>,
-    settings: <i className="fas fa-cog"></i>
+  dashboard: <i className="fas fa-th-large" />,
+  containers: <i className="fas fa-database fa-lg" />,
+  team: <i className="fas fa-users" />,
+  settings: <i className="fas fa-cog" />,
 };
 
 const IconHeader = ({ title, type }) => (
-    <div className="iconheader">
-        {icons[type]}
-        <h1 className="header">{title}</h1>
-    </div>
+  <div className="iconheader">
+    {icons[type]}
+    <h1 className="header">{title}</h1>
+  </div>
 );
 
 IconHeader.propTypes = {
-    title: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
 };
 
 export default IconHeader;
