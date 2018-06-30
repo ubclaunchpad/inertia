7a75d172d92391d45ae9ad6db997b0571266553e
Robert Lin <robertlin1@gmail.com>
2018-06-30 12:17:19 -0700

    gofmt -s

--- daemon/inertiad/auth/sessions_test.go
+++ daemon/inertiad/auth/sessions_test.go
@@ -10,10 +10,10 @@ import (
 func Test_sessionManager_EndAllSessions(t *testing.T) {
 	// make two sessions because map is pointer
 	sessions := map[string]*crypto.TokenClaims{
-		"1234": &crypto.TokenClaims{User: "bob"},
+		"1234": {User: "bob"},
 	}
 	manager := &sessionManager{internal: map[string]*crypto.TokenClaims{
-		"1234": &crypto.TokenClaims{User: "bob"},
+		"1234": {User: "bob"},
 	}}
 	manager.EndAllSessions()
 	assert.False(t, assert.ObjectsAreEqualValues(sessions, manager.internal))
@@ -22,10 +22,10 @@ func Test_sessionManager_EndAllSessions(t *testing.T) {
 func Test_sessionManager_EndAllUserSessions(t *testing.T) {
 	// make two sessions because map is pointer
 	sessions := map[string]*crypto.TokenClaims{
-		"1234": &crypto.TokenClaims{User: "bob"},
+		"1234": {User: "bob"},
 	}
 	manager := &sessionManager{internal: map[string]*crypto.TokenClaims{
-		"1234": &crypto.TokenClaims{User: "bob"},
+		"1234": {User: "bob"},
 	}}
 	manager.EndAllUserSessions("bob")
 	assert.False(t, assert.ObjectsAreEqualValues(sessions, manager.internal))
