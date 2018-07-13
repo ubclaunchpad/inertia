7d6281880d342f954e1e1d0b59c29f1eb7635e71
Robert Lin <robertlin1@gmail.com>
2018-07-12 20:53:22 -0700

    Add basic container ops tests

--- daemon/inertiad/containers/containers_test.go
+++ daemon/inertiad/containers/containers_test.go
@@ -2,15 +2,47 @@ package containers
 
 import (
 	"context"
+	"os"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/docker/docker/api/types"
 	docker "github.com/docker/docker/client"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCleanup(t *testing.T) {
+func TestContainerLogs(t *testing.T) {
+	cli, err := docker.NewEnvClient()
+	assert.Nil(t, err)
+	defer cli.Close()
+
+	_, err = ContainerLogs(cli, LogOptions{Container: "/testvps"})
+	assert.Nil(t, err)
+}
+
+func TestStreamContainerLogs(t *testing.T) {
+	cli, err := docker.NewEnvClient()
+	assert.Nil(t, err)
+	defer cli.Close()
+
+	// todo: flesh this out a bit more
+	stop := make(chan struct{})
+	go StreamContainerLogs(cli, "/testvps", os.Stdout, stop)
+	time.Sleep(1 * time.Second)
+	close(stop)
+}
+
+func TestGetActiveContainers(t *testing.T) {
+	cli, err := docker.NewEnvClient()
+	assert.Nil(t, err)
+	defer cli.Close()
+
+	_, err = GetActiveContainers(cli)
+	assert.Nil(t, err)
+}
+
+func TestPrune(t *testing.T) {
 	cli, err := docker.NewEnvClient()
 	assert.Nil(t, err)
 	defer cli.Close()
