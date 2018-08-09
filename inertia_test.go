90dd8beffea25c2c27a8db4faeda881d499b6289
Robert Lin <robertlin1@gmail.com>
2018-08-08 19:02:09 -0700

    Inject version var directly into package cmd

--- inertia_test.go
+++ inertia_test.go
@@ -2,19 +2,9 @@ package main
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/ubclaunchpad/inertia/cmd"
 )
 
+// This simply tests that everything goes smoothly
 func TestMain(t *testing.T) {
 	main()
-	assert.Equal(t, "latest", cmd.Root.Version)
-}
-
-func TestMainSetVersion(t *testing.T) {
-	Version = "robert"
-	main()
-	assert.Equal(t, Version, cmd.Root.Version)
-	Version = ""
 }
