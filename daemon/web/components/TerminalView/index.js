cc46f8f20b89e2a72fcad00ecca7fc0b1e40bb3e
Anna Situ <annasitu1@gmail.com>
2018-08-28 17:29:50 -0700

    Terminal styling changes

--- daemon/web/components/TerminalView/index.js
+++ daemon/web/components/TerminalView/index.js
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 import './index.sass';
 
 const TerminalView = ({ logs }) => (
-  <div className="terminal-view">
+  <div className="terminal-view fill-width height-xxxl">
     <textarea
-      className="text-area"
+      className="text-area pad-sides-s pad-ends-xxs fill-height fill-width bg-terminal"
       readOnly
       value={!logs ? '' : logs.reduce((accumulator, currentVal) => accumulator + '\r\n' + currentVal)} />
   </div>
