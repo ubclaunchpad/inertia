c4f4731308fca65922fb13f3c101c348a20e7eeb
Kieran Halliday <kieranhalliday@Kierans-MacBook-Pro.local>
2018-11-17 10:33:05 -0800

    Added command line support for the enable and disable totp endpoints

--- client/client.go
+++ client/client.go
@@ -364,6 +364,22 @@ func (c *Client) ListUsers() (*http.Response, error) {
 	return c.get("/user/list", nil)
 }
 
+// EnableTotp enables Totp for a given user
+func (c *Client) EnableTotp(username, password string) (*http.Response, error) {
+	return c.post("/user/totp/enable", &common.UserRequest{
+		Username: username,
+		Password: password,
+	})
+}
+
+// DisableTotp enables Totp for a given user
+func (c *Client) DisableTotp(username, password string) (*http.Response, error) {
+	return c.post("/user/totp/disable", &common.UserRequest{
+		Username: username,
+		Password: password,
+	})
+}
+
 // Sends a GET request. "queries" contains query string arguments.
 func (c *Client) get(endpoint string, queries map[string]string) (*http.Response, error) {
 	// Assemble request
