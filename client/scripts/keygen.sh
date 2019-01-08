ac92df8b2669492cb09a8993d803f590fbb22f02
Robert Lin <robertlin1@gmail.com>
2019-01-08 11:37:32 -0800

    client/scripts: lint keygen.sh

--- client/scripts/keygen.sh
+++ client/scripts/keygen.sh
@@ -7,17 +7,17 @@ set -e
 ID_DESTINATION=$HOME/.ssh/id_rsa_inertia_deploy
 PUB_ID_DESTINATION=$HOME/.ssh/id_rsa_inertia_deploy.pub
 
-# Check if destination file already exists:
-if [ -f $ID_DESTINATION ]; then
-    if [ ! -f $PUB_ID_DESTINATION ]; then
+# Check if destination file already exists
+if [ -f "$ID_DESTINATION" ]; then
+    if [ ! -f "$PUB_ID_DESTINATION" ]; then
         # If public key doesnt exist, make it.
-        ssh-keygen -y -f $ID_DESTINATION > $PUB_ID_DESTINATION
+        ssh-keygen -y -f "$ID_DESTINATION" > "$PUB_ID_DESTINATION"
     fi;
 else
     # Generate key with no password.
-    ssh-keygen -f $ID_DESTINATION -t rsa -N ''
+    ssh-keygen -f "$ID_DESTINATION" -t rsa -N ''
 fi
 
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 
-cat $PUB_ID_DESTINATION
+cat "$PUB_ID_DESTINATION"
