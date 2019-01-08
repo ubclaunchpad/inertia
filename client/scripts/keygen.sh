ad15fabfd33d31c8ef4e40294086eb12ba228eb0
Robert Lin <robertlin1@gmail.com>
2019-01-08 11:39:16 -0800

    client/scripts: install openssh if ssh-keygen is not available

--- client/scripts/keygen.sh
+++ client/scripts/keygen.sh
@@ -7,6 +7,11 @@ set -e
 ID_DESTINATION=$HOME/.ssh/id_rsa_inertia_deploy
 PUB_ID_DESTINATION=$HOME/.ssh/id_rsa_inertia_deploy.pub
 
+# Install openssh if ssh-keygen is not available
+if hash ssh-keygen 2>/dev/null; then
+    sudo apt-get install openssh-client || sudo apt install openssh-client
+fi;
+
 # Check if destination file already exists
 if [ -f "$ID_DESTINATION" ]; then
     if [ ! -f "$PUB_ID_DESTINATION" ]; then
