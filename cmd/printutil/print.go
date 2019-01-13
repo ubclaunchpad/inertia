e57a2cfc70781b05d98d5b77152757a93d4dc4dd
Robert Lin <robertlin1@gmail.com>
2019-01-13 13:40:22 -0800

    Add godocs for Fatal and Fatalf

--- cmd/printutil/print.go
+++ cmd/printutil/print.go
@@ -5,12 +5,14 @@ import (
 	"os"
 )
 
+// Fatal is a wrapper around fmt.Print that exits with status 1
 func Fatal(args ...interface{}) {
 	fmt.Print(args...)
 	println()
 	os.Exit(1)
 }
 
+// Fatalf is a wrapper around fmt.Printf that exits with status 1
 func Fatalf(format string, args ...interface{}) {
 	fmt.Printf(format, args...)
 	os.Exit(1)
