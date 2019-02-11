697c3332f9239483a60de10cfaaea09dcf14285d
Karim <kimoantiqe@hotmail.com>
2019-02-10 20:21:21 -0800

    Added preinstall script to npm

---
+++ .npm/src/copyREADME.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+
+const fs = require("fs-extra"); 
+
+
+fs.copy('../README.md', 'README.md', (err) => {
+  if (err) throw err;
+});
+
+fs.copy('../.static', '.static', (err) => {
+  if (err) throw err;
+});
