35b7b81c6ea3ce0fce5eeda61a43a34a0b83a070
yaoharry <harryyao13@gmail.com>
2019-02-12 12:34:19 -0800

    Migrated home path checking to local folder

--- local/env.go
+++ local/env.go
@@ -1,6 +1,42 @@
 package local
 
+import (
+	"errors"
+	"os"
+	"runtime"
+)
+
 const (
 	// EnvSSHPassphrase is the key used to fetch PEM key passphrases
 	EnvSSHPassphrase = "PEM_PASSPHRASE"
 )
+
+// GetHomePath returns the homepath based on the operating system used
+func GetHomePath() (string, error) {
+	if runtime.GOOS == "windows" {
+		// Performs check for HOME env variable first
+		if home := os.Getenv("HOME"); home != "" {
+			return home, nil
+		}
+
+		// Performs check for USERPROFILE as backup
+		if home := os.Getenv("USERPROFILE"); home != "" {
+			return home, nil
+		}
+
+		// Builds HOME from HOMEDRIVE and HOMEPATH as default
+		drive := os.Getenv("HOMEDRIVE")
+		path := os.Getenv("HOMEPATH")
+		home := drive + path
+		if drive == "" || path == "" {
+			return "", errors.New("HOMEDRIVE, HOMEPATH, or USERPROFILE environment variables are blank")
+		}
+
+		return home, nil
+	}
+	// Unix system as default
+	if home := os.Getenv("HOME"); home != "" {
+		return home, nil
+	}
+	return "", errors.New("HOME environment variable is blank")
+}
---
+++ local/env_test.go
@@ -0,0 +1,13 @@
+package local
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetHomePath(t *testing.T) {
+	env, err := GetHomePath()
+	assert.NoError(t, err)
+	assert.DirExists(t, env)
+}
