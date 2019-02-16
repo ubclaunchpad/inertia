96feb9544d73c2b62049fff19f6967f46263befc
Karim <kimoantiqe@hotmail.com>
2019-02-16 13:49:50 -0800

    finallized package.json

--- contrib/npm/src/copyAssets.js
+++ contrib/npm/src/copyAssets.js
@@ -2,10 +2,10 @@
 
 const fs = require('fs-extra');
 
-fs.copy('../README.md', 'README.md', (err) => {
+fs.copy('../../README.md', 'README.md', (err) => {
   if (err) throw err;
 });
 
-fs.copy('../.static', '.static', (err) => {
+fs.copy('../../.static', '.static', (err) => {
   if (err) throw err;
 });
