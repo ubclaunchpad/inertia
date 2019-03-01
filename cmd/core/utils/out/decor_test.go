f763f74ae4db78e2d6e066de8a395260b8ef9990
Robert Lin <robertlin1@gmail.com>
2019-03-01 14:23:50 -0800

    improve decor test

--- cmd/core/utils/out/decor_test.go
+++ cmd/core/utils/out/decor_test.go
@@ -4,12 +4,16 @@ import (
 	"testing"
 
 	"github.com/fatih/color"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPrint(t *testing.T) {
 	Printf("hello world %s :rocket:", "yaoharry")
 	Print("hello world :rocket: ?? ")
 	Println("hello world :rocket:")
+	assert.Contains(t,
+		Sprintf("am I :rocket:"),
+		"\U0001f680")
 	// uncomment the following to see the emoji output
 	// t.Fail()
 }
@@ -17,7 +21,9 @@ func TestPrint(t *testing.T) {
 func TestColor(t *testing.T) {
 	Printf("am I %s?\n", Color{C: color.New(color.FgRed), S: "coloured"})
 	Printf(":rocket: am I %s?\n", C("coloured :rocket:", RD, UL))
-	Printf(":rocket: am I %s?\n", C("coloured %s", RD, UL).With(":rocket:"))
+	assert.Contains(t,
+		Sprintf("am I %s?\n", C("coloured %s", RD, UL).With(":rocket:")),
+		"\U0001f680")
 	// uncomment the following to see the coloured output
-	t.Fail()
+	// t.Fail()
 }
